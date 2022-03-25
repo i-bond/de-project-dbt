64650957bcc55e809dbddb9cc93bf3c4069ce0da
Ilya Bondarev <jackhoover88@yandex.com>
2022-03-25 16:31:40 +0000

    dbt models created

---
+++ models/core/netflix_movies_final.sql
@@ -0,0 +1,10 @@
+{{ config(materialized='table') }}
+
+
+select
+    title,
+    year_of_release,
+    round(avg(rating), 2) as avg_rating,
+    count(rating) as votes_number
+from {{ ref('netflix_movies') }}
+group by title, year_of_release 
